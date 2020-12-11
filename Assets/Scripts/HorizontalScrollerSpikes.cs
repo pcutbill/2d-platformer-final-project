2731b26dafb9996b18c6e15244014da93005482e
pcutbill <70859466+pcutbill@users.noreply.github.com>
2020-12-11 10:25:05 -0600

    Mapped out Level 4

---
+++ Assets/Scripts/HorizontalScrollerSpikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalScrollerSpikes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float step = 0.002f;
+        var cameraPosition = transform.position;
+        cameraPosition.x += step;
+        if(cameraPosition.x > 150f)
+        {
+            cameraPosition.x = 150f;
+            this.gameObject.SetActive(false);
+        }
+
+        transform.position = cameraPosition;
+    }
+}
