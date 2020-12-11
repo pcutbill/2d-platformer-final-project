3d2494528e1933e88b33afabff96516f7185d504
pcutbill <70859466+pcutbill@users.noreply.github.com>
2020-12-10 22:48:13 -0600

    Fixed Tile Map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed = 50f;
+    public float runMultiplier = 1.0f;
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -17,12 +18,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Run"))
+        {
+            runMultiplier = 1.5f;
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
         }
-        Vector2 force = new Vector2((Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed),0);
+        Vector2 force = new Vector2((Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed * runMultiplier),0);
 
         rb.AddForce(force);
     }
