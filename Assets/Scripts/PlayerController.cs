e08a8949cb001698a0cc325deffc8ebbfaf9e950
pcutbill <70859466+pcutbill@users.noreply.github.com>
2020-12-10 18:31:29 -0600

    primitive player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), 0f);
+        rb.MovePosition(rb.position += movement * Time.deltaTime * moveSpeed);
+    }
+}
