578c384b08635b731f00d71d2d8cc44690f9e60f
pcutbill <70859466+pcutbill@users.noreply.github.com>
2020-12-11 00:17:59 -0600

    Fixed player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 50f;
     public float runMultiplier = 1.0f;
     public Rigidbody2D rb;
+    bool inAir = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +24,21 @@ public class PlayerController : MonoBehaviour
             runMultiplier = 1.5f;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !inAir)
         {
             rb.AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
+            inAir = true;
         }
         Vector2 force = new Vector2((Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed * runMultiplier),0);
 
         rb.AddForce(force);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            inAir = false;
+        }
     }
 }
