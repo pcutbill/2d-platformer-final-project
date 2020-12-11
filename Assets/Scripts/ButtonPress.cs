a959fdb7955a168f08a28694d9d7f578bd70b54c
pcutbill <70859466+pcutbill@users.noreply.github.com>
2020-12-11 11:06:13 -0600

    level 4 without enemies done

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ButtonPress : MonoBehaviour
 {
+    public AudioClip dropSound;
+    public AudioClip pressSound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,29 @@ public class ButtonPress : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator setOffTrap()
+    {
+        yield return new WaitForSeconds(.25f);
+        int trapNum = 1;
+        while (trapNum < 11)
+        {
+            AudioSource.PlayClipAtPoint(dropSound, transform.position);
+            yield return new WaitForSeconds(.25f);
+            GameObject.Find("BossTrap" + trapNum).GetComponent<Rigidbody2D>().isKinematic = false;
+            trapNum++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("PlayerTemp").GetComponent<Rigidbody2D>().isKinematic = true;
+            GameObject.Find("PlayerTemp").GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            AudioSource.PlayClipAtPoint(pressSound, transform.position);
+            StartCoroutine(setOffTrap());
+        }
     }
 }
