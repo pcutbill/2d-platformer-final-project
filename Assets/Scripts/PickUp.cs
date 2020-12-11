e0156684ddc3602b8852ea7f382a011d4adc48c8
ccohu <camerontcohu@gmail.com>
2020-12-10 22:33:44 -0600

    Uhhh

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
